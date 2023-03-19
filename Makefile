40a05a081b137f91381f5da88c44dfbb6f2c075b
Alex967CW <awcyclist3@gmail.com>
2023-03-18 22:03:48 -0400

    Add basic ustove interface
    - Enabled user to enter expressions and see pretty printed result
    - Added exception handling in parsing commands
    - Added simple help menu and ustove commands
    - Edited Makefile to include "make ustove" command

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ build:
 utop:
 	dune utop src
 
+ustove:
+	OCAMLRUNPARAM=b dune exec bin/main.exe
+
 .PHONY: test
 test:
 	dune exec test/main.exe
---
+++ bin/main.ml
@@ -0,0 +1,41 @@
+let rec take_commands () =
+  print_string "> ";
+  match read_line () with
+  | "" -> take_commands ()
+  | x -> (
+      if String.get x 0 = '#' then
+        ustring_commands (String.sub x 1 (String.length x - 1))
+      else
+        match
+          let parsed = Interp.Main.parse x in
+          Interp.Main.pretty_print parsed 1
+        with
+        | exception End_of_file -> ()
+        | exception e ->
+            print_string "Error: ";
+            print_endline (Printexc.to_string e);
+            print_endline "";
+            take_commands ()
+        | x ->
+            print_string x;
+            print_endline "";
+            take_commands ())
+
+and ustring_commands = function
+  | "quit" -> Stdlib.exit 0
+  | "ustove_help" ->
+      print_endline "\nustove defines the following directives:";
+      print_string
+        "#ustove_help : list all directives \n#quit : quits session \n\n";
+      take_commands ()
+  | _ ->
+      print_endline "Invalid command.";
+      take_commands ()
+
+let main () =
+  ANSITerminal.print_string [ ANSITerminal.red ] "\n\nWelcome to ustove!\n";
+  print_endline "Type #ustove_help for help about using ustove.";
+  print_endline "Please enter a command.";
+  take_commands ()
+
+let () = main ()
