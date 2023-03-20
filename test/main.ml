1f1b28f2e14f0c466478e770bbe163d4a00eb3b2
zachseidner1 <zachary.seidner@gmail.com>
2023-03-20 19:46:14 -0400

    Basic testing framework - Reorganized testing into separate lists with List.flatten - Allows for greater organization for testing

--- test/main.ml
+++ test/main.ml
@@ -14,13 +14,10 @@ let eval_string_expression_test name expected_output string_expression =
   name >:: fun _ ->
   assert_equal expected_output (interp string_expression) ~printer:id
 
+let parse_test (name : string) (input : string) (expected_output : Ast.expr) =
+  name >:: fun _ -> assert_equal (parse input) expected_output
 
-
-let parse_test (name: string) (input: string) (expected_output: Ast.expr) =
-  name >:: fun _ ->
-    assert_equal (parse input) expected_output
-
-let parse_tests =
+let eval_int_tests =
   [
     eval_int_expression_test "0 should parse to 0" 0 "0";
     eval_int_expression_test "6 should parse to 6" 6 "6";
@@ -29,23 +26,29 @@ let parse_tests =
     eval_int_expression_test "5 fk 9 should parse to 12" 12 "5 fk 9";
     eval_int_expression_test "2 + 3 * 10 should parse to 32" 32 "2+3*10";
     eval_int_expression_test "2 * 10 + 2 should parse to 22" 22 "2*10+2";
-    eval_int_expression_test "2 * (10 + 2) should parse to 24" 24
-      "2 * (10 + 2)";
+    eval_int_expression_test "2 * (10 + 2) should parse to 24" 24 "2 * (10 + 2)";
+  ]
 
+let eval_float_tests =
+  [
     eval_float_expression_test "2.0 should parse to 2.0" 2.0 "2.0";
     eval_float_expression_test "-7.0 should parse to 2.0" ~-.7.0 "-7.0";
     eval_float_expression_test "PIE should parse to 3.141..." Float.pi "PIE";
+  ]
 
+let eval_string_tests =
+  [
     eval_string_expression_test "\"a\" should parse to a" "a" "\"a\"";
     eval_string_expression_test "\"abcde\" + \"a\" should parse to abcdea"
       "abcdea" "\"abcde\" + \"a\"";
     eval_string_expression_test "\"a\" + 1 should parse to a1" "a1" "\"a\" + 1";
     eval_string_expression_test "" "2a31" "1 + 1 + \"a\" + 3 + 1";
-]
+  ]
 
-let parse_tests = [
-  parse_test "the number 1" "1" (Cal 1)
-]
+let eval_tests =
+  List.flatten [ eval_int_tests; eval_float_tests; eval_string_tests ]
 
-let () = run_test_tt_main ("suite" >::: parse_tests);
-run_test_tt_main ("parse tests" >::: parse_tests);
+let parse_int_tests = [ parse_test "parse 1" "1" (Cal 1) ]
+let parse_tests = List.flatten [ parse_int_tests ]
+let tests = List.flatten [ eval_tests; parse_tests ]
+let () = run_test_tt_main ("suite" >::: tests)
