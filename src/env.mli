0ee4e9d10c8e37a42d762736ae8123d3e388bc82
Alex Kozik <ajk333@cornell.edu>
2023-04-19 19:04:18 -0400

    Worked on env.mli

--- src/env.mli
+++ src/env.mli
@@ -1,5 +1,9 @@
-type t (* t represents an environment *)
-type expression (* represents an expression *)
-type value (* represents a value *)
+open Ast
 
-val eval : t -> expression -> value
+type t
+type binding_value
+
+val empty : t (* an environment with no bindings *)
+val remove_binding : string -> t -> t
+val add_binding : string -> binding_value -> t -> t
+val get_binding : string -> t -> binding_value option
