43bab94a8b8c92171285ea86b656837dae3d2335
zachseidner1 <zachary.seidner@gmail.com>
2023-04-26 15:04:56 -0400

    Use negation of equals instead of not =

--- src/env.mli
+++ src/env.mli
@@ -15,3 +15,4 @@ val empty : t (* an environment with no bindings *)
 val remove_binding : string -> t -> t
 val add_binding : string -> binding_value -> t -> t
 val get_binding : string -> t -> binding_value option
+val to_string : t -> string
